4deb7d46ee9df5da0f11649cbd1fb7336f13afcb
Seagull-D <djseagull90@gmail.com>
2023-10-04 21:24:44 +0200

    Update task-01.js

---
+++ js/task-01.js
@@ -0,0 +1,5 @@
+const iteams = document.querySelectorAll(".item");
+// console.log(iteams);
+const iteamArr = [...iteams];
+console.log(`Number of categories: ${iteamArr.length}`);
+console.log(iteamArr.text);
