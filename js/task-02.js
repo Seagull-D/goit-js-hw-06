28ead9cac03508e683e92c12471f772f45e2b0ef
Seagull-D <djseagull90@gmail.com>
2023-10-05 20:18:23 +0200

    task 2 end
    forEach, createElement, apped

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,18 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const list = document.querySelector("#ingredients");
+
+ingredients.forEach((iteam) => {
+  console.log(iteam);
+  const createIteam = document.createElement("li");
+  createIteam.textContent = iteam;
+  list.append(createIteam);
+});
+// console.log(document);
