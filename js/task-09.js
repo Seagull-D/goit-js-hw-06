c43ebd64f3f1b9d904480c94d33245b08570add2
Seagull-D <djseagull90@gmail.com>
2023-10-10 19:15:51 +0200

    9 end

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const changeBtn = document.querySelector(".change-color");
+console.log(changeBtn);
+
+changeBtn.addEventListener("click", ColorChange);
+function ColorChange(evt) {
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+}
