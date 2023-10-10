28d0e4eb61147c4a379ce3883b810ca1a13286c8
Seagull-D <djseagull90@gmail.com>
2023-10-10 19:05:28 +0200

    8 task end

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const userForm = document.querySelector(".login-form");
+const userData = {};
+userForm.addEventListener("submit", onFormSubmit);
 
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  const userEmail = evt.currentTarget.elements.email.value;
+  const userPassword = evt.currentTarget.elements.password.value;
+
+  if (userEmail.length > 0 && userPassword.length > 0) {
+    userData.email = userEmail;
+    userData.password = userPassword;
+    console.log(userData);
+  } else {
+    alert("Are you joke?");
+  }
+  userForm.reset();
+}
